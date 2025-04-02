79005a6b83b8171386cc7d990134e6fce68dbf2e
Sergey Dreger <sergey.dreger@gmail.com>
2025-04-02 08:50:08 +0300

    chore: add basic project structure generated by Vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import bookShelfLogo from '/bookshelf.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={bookShelfLogo} className="logo" alt="Bookshelf logo" />
+      </div>
+      <h1>Personal Library Manager</h1>
+    </>
+  )
+}
+
+export default App
